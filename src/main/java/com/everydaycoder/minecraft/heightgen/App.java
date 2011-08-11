6722ce89978423c8c632b6483ca1291740adfd24
alyawn <alyawn@gmail.com>
2011-08-11 09:57:45 -0400

    Added maven project files

---
+++ src/main/java/com/everydaycoder/minecraft/heightgen/App.java
@@ -0,0 +1,13 @@
+package com.everydaycoder.minecraft.heightgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
